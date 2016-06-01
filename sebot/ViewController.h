2a73dcfdd870f8b255c6d0381205d0654409eaf4
ldp <da_peng@100yibuy.com>
2016-06-01 16:32:17 +0800

    init project

---
+++ sebot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sebot
+//
+//  Created by ldp on 16/6/1.
+//  Copyright © 2016年 sego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
