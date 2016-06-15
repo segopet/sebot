fe8a3bd7fe7405ba6b133cb269609c8bbc7f2408
yuleicoco <1040341999@qq.com>
2016-06-15 14:23:42 +0800

    no message

---
+++ sebot/Classes/Common/Macro/Notification.h
@@ -0,0 +1,15 @@
+//
+//  Notification.h
+//  sebot
+//
+//  Created by yulei on 16/6/15.
+//  Copyright © 2016年 sego. All rights reserved.
+//
+
+#ifndef Notification_h
+#define Notification_h
+
+
+#endif /* Notification_h */
+
+static NSString *NotificationLoginStateChange = @"NotificationLoginStateChange";
