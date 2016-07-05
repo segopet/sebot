9b912634f1030a36beb3f6865fca6f1f25b6a896
ChenZeXuan <610121499@qq.com>
2016-07-05 17:38:59 +0800

    。。先提交

--- sebot/Classes/Network/AFHttpClient+Account.h
+++ sebot/Classes/Network/AFHttpClient+Account.h
@@ -12,7 +12,7 @@
 
 
 
-- (void)loginWithUserName:(NSString*)userName password:(NSString*)password userid:(NSString *)userid  complete:(void(^)(ResponseModel *model))completeBlock;
+- (void)loginWithUserName:(NSString*)userName password:(NSString*)password userid:(NSString *)userid complete:(void(^)(ResponseModel *model))completeBlock;
 
 
 
