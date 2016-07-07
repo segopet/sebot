2d20a280d3503ea4cee966354481364f1933d1e3
ChenZeXuan <610121499@qq.com>
2016-07-07 14:54:29 +0800

    先提交

---
+++ sebot/Classes/ViewControlls/MainTab/FamilySpace/IssueViewController.h
@@ -0,0 +1,13 @@
+//
+//  IssueViewController.h
+//  sebot
+//
+//  Created by czx on 16/7/6.
+//  Copyright © 2016年 sego. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface IssueViewController : BaseViewController
+@property (nonatomic,strong)UIImage * firstImage;
+@end
